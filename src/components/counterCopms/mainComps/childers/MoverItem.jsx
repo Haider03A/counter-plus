479945d99bfb0750b485504bed6165697d8414b6
Haider <79869890+Haider03A@users.noreply.github.com>
2023-09-03 21:31:28 +0300

    to phone

--- src/components/counterCopms/mainComps/childers/MoverItem.jsx
+++ src/components/counterCopms/mainComps/childers/MoverItem.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useState } from "react"
 
-export default ({ listBoxRef }) => {
+
+export default ({ listBoxRef, items }) => {
+
+    const [itemMoved, setItemMoved] = useState(false)
+
     let timeHoldTouch, timeTouchStatus, parentElement
 
-    const touchStartHandler = (e) => timeHoldTouch = setTimeout(() => timeTouchStatus = true, 300)
+    const touchStartHandler = (e) => {
+        timeHoldTouch = setTimeout(() => {
+            timeTouchStatus = true
+            parentElement = e.target.parentElement
+        }, 300)
+    }
 
     const touchEndHandler = (e) => {
-        if(timeTouchStatus){
+        if (timeTouchStatus) {
             parentElement.style.transform = null
-        } else { 
+            parentElement.style.borderColor = null
+            parentElement.style.zIndex = null
+            setItemMoved(false)
+        } else {
             clearTimeout(timeHoldTouch)
         }
     }
@@ -17,7 +29,7 @@ export default ({ listBoxRef }) => {
 
     const touchMoveHandler = (e) => {
         if (timeTouchStatus) {
-            parentElement = e.target.parentElement
+
             const moverHeight = e.target.offsetHeight
             const itemHeight = parentElement.offsetHeight
             const itemOffsetY = parentElement.offsetTop
@@ -25,6 +37,8 @@ export default ({ listBoxRef }) => {
             const result = -(itemOffsetY + (itemHeight - moverHeight / 2) - pageY)
 
             parentElement.style.transform = `translateY(${result}px) scale(0.9)`
+            parentElement.style.borderColor = 'rgb(129 140 248)'
+            parentElement.style.zIndex = 3
 
             const clientY = e.touches[0].clientY
             const screenHeight = window.screen.height
@@ -33,7 +47,12 @@ export default ({ listBoxRef }) => {
                 window.scrollBy(0, -10)
             } else if (clientY > screenHeight - 200) {
                 window.scrollBy(0, +10)
-            }
+            };
+
+            // const itemCountInBox = items.length
+            // const itemHeightInBox = itemCountInBox*itemHeight
+
+            // setItemMoved(true);
 
         } else {
             clearTimeout(timeHoldTouch)
@@ -44,7 +63,7 @@ export default ({ listBoxRef }) => {
 
     return (
         <div onTouchMove={touchMoveHandler} onTouchStart={touchStartHandler} onTouchEnd={touchEndHandler} className='focus:outline-none touch-none py-2 pt-3 mt-3 w-full'>
-            <span className='block pointer-events-none mx-auto w-9 h-1 rounded-lg bg-gray-300'></span>
+            <span className={`${itemMoved ? 'bg-indigo-400' : 'bg-gray-300'} block pointer-events-none mx-auto w-9 h-1 rounded-lg`}></span>
         </div>
 
     )
